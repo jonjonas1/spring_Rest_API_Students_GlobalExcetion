740510739b9dec7d04bfc16a134ce727338ceda0
ojuraev@hotmail.com <ojuraev@hotmail.com>
2022-01-20 21:40:36 -0600

    rest api with global exception

--- src/main/java/com/springdemo/rest/StudentRestController.java
+++ src/main/java/com/springdemo/rest/StudentRestController.java
@@ -3,9 +3,6 @@ package com.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,7 +38,6 @@ public class StudentRestController {
 		return theStudents;
 	}
 
-	// Path Variables: returning only unique students by passing path variable
 	@GetMapping("/students/{studentId}")
 	public Student getStudents(@PathVariable int studentId) {
 		
@@ -52,31 +48,5 @@ public class StudentRestController {
 		return theStudents.get(studentId);
 	}
 	
-	// Add an exception handler using @ExceptionHandler
-	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
-		//create a StudentErrorResponse
-		StudentErrorResponse error = new StudentErrorResponse();
-		
-		error.setStatus(HttpStatus.NOT_FOUND.value());
-		error.setMessage(exc.getMessage());
-		error.setTimeStamp(System.currentTimeMillis());
-		
-		//return ResponseEntity
-		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-	}
 	
-	
-	// add another exception handler .. to catch any exception, catch {String path} request
-	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
-		
-		StudentErrorResponse error = new StudentErrorResponse();
-		
-		error.setStatus(HttpStatus.BAD_REQUEST.value());
-		error.setMessage(exc.getMessage());
-		error.setTimeStamp(System.currentTimeMillis());
-		
-		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-	}	
 }
--- src/main/java/den/DFGdfg.java
+++
@@ -1,5 +0,0 @@
-package den;
-
-public class DFGdfg {
-
-}
